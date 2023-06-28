03df3bbc4400d866e832a171d809b0b20799630d
embotden <mm.botden@gmail.com>
2023-06-28 21:27:52 +0200

    update ending

--- Assets/ChooseScene.cs
+++ Assets/ChooseScene.cs
@@ -1,24 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.Sequences;
 
 public class ChooseScene : MonoBehaviour
 {
     InventorySystem inventorySystem;
+    countingMemories _allSeen;
+    public GameObject _blockControls;
+    PlayableDirector _finalPlayableDirectorRemember;
+    PlayableDirector _finalplayableDirectorForget;
+    public GameObject _ZimsThings;
+
     public SceneReference _Remember;
     public SceneReference _Forget;
+
+    public GameObject _decalRemember;
+    public GameObject _decalForget;
 
     // Start is called before the first frame update
     void Start()
     {
         inventorySystem = FindObjectOfType<InventorySystem>();
+
+        if(inventorySystem._hurtCanTrigger)
+        {
+            _decalRemember.SetActive(true);
+            Destroy(_decalForget);
+        }
+        else
+        {
+            _decalForget.SetActive(true);
+            Destroy(_decalRemember);
+        }
     }
 
-    void FinaleTriggerd(Scene scene)
+    private void Update()
     {
-        if(inventorySystem.IsInTrauma())
+        if(_allSeen._timeForCutscene)
+            StartTheCutscene();
+    }
+
+    void StartTheCutscene()
+    {
+        if (inventorySystem._hurtCanTrigger)
+            _finalPlayableDirectorRemember.Play();
+        else
+            _finalplayableDirectorForget.Play();
+    }
+
+    public void FinaleTriggerd()
+    {
+        if(inventorySystem._hurtCanTrigger)
         {
             SceneManager.LoadSceneAsync(_Remember, LoadSceneMode.Single);
         }
